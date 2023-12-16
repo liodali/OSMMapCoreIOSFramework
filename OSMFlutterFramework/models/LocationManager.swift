cd82117ecf2c57147edf458ac43c43156f96fbe8
liodali <lionelviperdali@gmail.com>
2023-12-16 22:12:45 +0100

    create LocationManager to manage UserLocation

---
+++ OSMFlutterFramework/models/LocationManager.swift
@@ -0,0 +1,151 @@
+//
+//  LocationManager.swift
+//  OSMFlutterFramework
+//
+//  Created by Dali Hamza on 16.12.23.
+//
+
+import Foundation
+import CoreLocation
+@_implementationOnly import MapCore
+import MapKit
+public enum LocationPermission {
+    case Granted
+    case NotGranted
+}
+public protocol OSMUserLocationHandler {
+    func locationChanged(userLocation:CLLocationCoordinate2D)
+    func handlePermission(state:LocationPermission)
+}
+public class LocationManager: NSObject, CLLocationManagerDelegate {
+    private let locationManager: CLLocationManager
+    private let map: MCMapView
+    var userLocationHandler:OSMUserLocationHandler?
+    private var isSingleRetrieve = false
+    private var isTracking = false
+    private var userLocationIcons:UserLocationConfiguration
+    private let iconLayer = MCIconLayerInterface.create()
+    private var userMarker:Marker?
+    init(map: MCMapView,userLocationIcons:UserLocationConfiguration?) {
+        self.map = map
+        self.locationManager = CLLocationManager()
+        self.userLocationIcons = userLocationIcons ?? UserLocationConfiguration(
+            userIcon: LocationManager.pinIcon(),
+            directionIcon: LocationManager.directionIcon()
+        )
+        super.init()
+        self.locationManager.delegate = self
+        iconLayer?.setLayerClickable(false)
+        self.map.insert(layer: iconLayer?.asLayerInterface(), at: 3)
+    }
+    public func setUserLocationIcons(userLocationIcons:UserLocationConfiguration) {
+        self.userLocationIcons = userLocationIcons
+    }
+    public func requestSingleLocation() {
+           locationManager.requestWhenInUseAuthorization() // Request permission
+           // Start location updates
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           requestLocation()
+           isSingleRetrieve = true
+    }
+    func requestLocation() {
+        if #available(iOS 14.0, *) {
+            if(locationManager.authorizationStatus == CLAuthorizationStatus.authorizedWhenInUse ) {
+                locationManager.requestLocation()
+            }
+        } else {
+            if(CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ) {
+                locationManager.requestLocation()
+            }
+        }
+    }
+    public func requestEnableLocation() {
+        locationManager.requestWhenInUseAuthorization() // Request permission
+        // Start location updates
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        requestLocation()
+    }
+    public func toggleTracking() {
+        isTracking = !isTracking
+        if !isTracking {
+            stopLocation()
+            iconLayer?.clear()
+            iconLayer?.invalidate()
+            userMarker = nil
+        }else {
+            locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
+        }
+    }
+    public func isTrackingEnabled()-> Bool {
+        isTracking
+    }
+    public func stopLocation() {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
+        isSingleRetrieve = false
+    }
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.last != nil && locations.last?.coordinate != nil && !isSingleRetrieve {
+            let mccoord = locations.last!.coordinate.toMCCoordEpsg3857()
+            self.map.camera.move(toCenterPosition: mccoord, animated: true)
+            if isTracking {
+                if iconLayer != nil && iconLayer!.getIcons().isEmpty {
+                    userMarker = Marker(location: locations.last!.coordinate, 
+                                        markerConfiguration: MarkerConfiguration(
+                                            icon: userLocationIcons.userIcon, iconSize: nil, angle: nil, anchor: nil
+                                        )
+                    )
+                    iconLayer?.add(userMarker?.createMapIcon()!)
+                }
+                let angle = manager.heading?.trueHeading
+                if (angle != nil && angle != 0) {
+                    userMarker?.updateIconMarker(configuration: userMarker!.markerConfiguration.copyWith(icon: userLocationIcons.directionIcon,
+                                                                                                         angle: Float(angle!), anchor: nil))
+                }
+                iconLayer?.getIcons().first?.setCoordinate(mccoord)
+            }
+        }
+        if let handler = userLocationHandler, locations.last != nil && locations.last?.coordinate != nil {
+            handler.locationChanged(userLocation: locations.last!.coordinate)
+        }
+        if isSingleRetrieve {
+            stopLocation()
+        }
+    }
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+
+        if status == CLAuthorizationStatus.authorizedWhenInUse || status == CLAuthorizationStatus.authorizedAlways  {
+            manager.requestLocation()
+            if  let handler = userLocationHandler {
+                handler.handlePermission(state: LocationPermission.Granted)
+            }
+        }else if status == CLAuthorizationStatus.denied || status == CLAuthorizationStatus.restricted {
+            if  let handler = userLocationHandler {
+                handler.handlePermission(state: LocationPermission.NotGranted)
+            }
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+        if  let handler = userLocationHandler {
+            
+        }
+    }
+     static func pinIcon() -> UIImage {
+         return (UIImage(systemName: "mappin") ?? UIImage()).withTintColor(.red)
+       }
+     static func directionIcon() -> UIImage {
+           return (UIImage(systemName: "location.north.fill") ?? UIImage()).withTintColor(.black)
+       }
+
+}
+public struct UserLocationConfiguration {
+    let userIcon:UIImage
+    let directionIcon:UIImage?
+    public init(userIcon: UIImage , directionIcon: UIImage?) {
+        self.userIcon = userIcon
+        self.directionIcon = directionIcon
+    }
+}
