c5f82fddb75e348e298d7d6893cac85ea4611cef
liodali <lionelviperdali@gmail.com>
2024-03-21 01:44:17 +0100

    update example project

--- example/OSMMapCoreFrameworkExample/OSMMapCoreFrameworkExample/ContentView.swift
+++ example/OSMMapCoreFrameworkExample/OSMMapCoreFrameworkExample/ContentView.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VMap()
     }
 }
-
+struct VMap :View {
+    var body: some View {
+        NavigationView {
+            VStack {
+               /* MapCoreNOSM(width: 250,height: 250)
+                .frame(width: 250,height: 250)
+                    .padding(EdgeInsets(top: 0, leading: 36, bottom: 32, trailing: 42))*/
+                MapCoreOSM(width: 500,height: 500)
+                .frame(width: 500,height: 500)
+                    .padding(EdgeInsets(top: 0, leading: 36, bottom: 65, trailing: 42))
+               
+              /*MapView()
+                    .padding(EdgeInsets(top: 0, leading: 36, bottom: 0, trailing: 0))*/
+                NavigationLink {
+                    VMap()
+                } label:{
+                    Text("open new map").padding(EdgeInsets(top: 0, leading: 0, bottom: 32, trailing: 0))
+                }
+            }
+            
+        }
+    }
+}
 #Preview {
     ContentView()
 }
