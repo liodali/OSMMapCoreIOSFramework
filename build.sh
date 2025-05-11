ba3541aee936e401acde3adc7bddd13eb9b16107
liodali <16631886+liodali@users.noreply.github.com>
2025-05-11 14:04:03 +0200

    fix build

--- build.sh
+++ build.sh
@@ -80,7 +80,7 @@ xcframeworkiphoneosBundle="${xcframeworklocation}/ios-arm64"
 xcframeworkiphonesimulatorBundle="${xcframeworklocation}/ios-arm64_x86_64-simulator"
 licence="${dir_project}/LICENSE"
 
-xcodebuild -workspace OSMFlutterFramework.xcworkspace -scheme OSMFlutterFramework  -create-xcframework -framework $frameworkiphoneos -framework $frameworkiphonesimulator -output $xcframeworklocation -UseModernBuildSystem=YES CONFIGURATION_BUILD_DIR=$dir_config_build
+xcodebuild -scheme OSMFlutterFramework  -create-xcframework -framework $frameworkiphoneos -framework $frameworkiphonesimulator -output $xcframeworklocation -UseModernBuildSystem=YES CONFIGURATION_BUILD_DIR=$dir_config_build
 
 if [ -d "${dir_build}/OSMFlutterFramework.xcframework" ]
 then
