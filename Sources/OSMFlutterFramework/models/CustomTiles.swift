c8fef2f0b5701f7b8a48383f8ff6aa62050ebeac
liodali <16631886+liodali@users.noreply.github.com>
2026-06-28 01:49:13 +0100

    add vector tile support to CustomTiles with styleURL handling and early return for vector tiles

--- Sources/OSMFlutterFramework/models/CustomTiles.swift
+++ Sources/OSMFlutterFramework/models/CustomTiles.swift
@@ -10,20 +10,34 @@ public class CustomTiles {
     var tileSize: String
     var maxZoom: String
     private(set) var randSubDomain = 0
-    public init(_ mapTile: [String: Any],isMapCore: Bool = true) {
+    public private(set) var isVector: Bool = false
+    public init(_ mapTile: [String: Any], isMapCore: Bool = true) {
+        let isVector = mapTile["isVector"] as? Bool ?? false
+        self.isVector = isVector
+
+        if isVector {
+            tileURL =
+                (mapTile["styleURL"] as? String)
+                ?? ((mapTile["urls"] as? [[String: Any]])?.first?["url"] as? String)
+                ?? ""
+            tileSize = (mapTile["tileSize"] as? Int)?.description ?? "256"
+            maxZoom = mapTile["maxZoomLevel"] as? String ?? "19"
+            return
+        }
+
         let tiles = (mapTile["urls"] as! [[String: Any]]).first
         tileURL = tiles!["url"] as! String
-         if !tileURL.contains("{z}") && !tileURL.contains("{y}") && !tileURL.contains("{x}") {
-             tileURL += "{z}/{x}/{y}"
+        if !tileURL.contains("{z}") && !tileURL.contains("{y}") && !tileURL.contains("{x}") {
+            tileURL += "{z}/{x}/{y}"
         }
         tileURL += (mapTile["tileExtension"] as! String)
         if isMapCore && ((tiles?.keys.contains("subdomains")) != nil) {
             let len = (tiles!["subdomains"] as? [String])?.count ?? 0
             if len != 0 {
-                randSubDomain = Int.random(in: 0...(len-1))
-                subDomains =   (tiles!["subdomains"] as? [String])?[randSubDomain] ?? ""
+                randSubDomain = Int.random(in: 0...(len - 1))
+                subDomains = (tiles!["subdomains"] as? [String])?[randSubDomain] ?? ""
             }
-        }else if !isMapCore && ((tiles?.keys.contains("subdomains")) != nil){
+        } else if !isMapCore && ((tiles?.keys.contains("subdomains")) != nil) {
             subDomains = (tiles!["subdomains"] as? [String])?.description ?? ""
         }
         tileSize = (mapTile["tileSize"] as? Int)?.description ?? "256"
@@ -34,11 +48,14 @@ public class CustomTiles {
         }
         maxZoom = mapTile["maxZoomLevel"] as? String ?? "19"
     }
-    public  func toString()-> String {
-        var url = tileURL
-        if (!subDomains.isEmpty){
-            url = url .replacingOccurrences(of: "{s}", with: "\(subDomains)")
+    public func toString() -> String {
+        if isVector {
+            return tileURL
         }
-       return  url
+        var url = tileURL
+        if !subDomains.isEmpty {
+            url = url.replacingOccurrences(of: "{s}", with: "\(subDomains)")
+        }
+        return url
     }
 }
